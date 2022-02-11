027b3c70a8baf83b9db6afd6034189f1ce9d8574
maryoud2000 <maryoud2000@gmail.com>
2022-02-11 20:41:35 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function Todo({ todo, index, ongoingTodo, completeTodo, removeTodo }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo"
+    style={{ backgroundColor: todo.isCompleted ? "green" : todo.isOnGoing ? "Yellow" : "" }}
+    >
+      {todo.text}
+      <div>    
+        <button onClick={() => ongoingTodo(index)}>Ongoing</button>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>Delete</button>
+      </div>
+    </div>
+  );
+};
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={event => setValue(event.target.value)}/>
+      <p>Enter new task</p>
+    </form>
+  );
+}
+
+function App() { 
+  const [todos, setTodos] = React.useState([]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text, isOnGoing: false, isCompleted: false }];
+    setTodos(newTodos);
+  };
+
+  const ongoingTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isOnGoing = true;
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  };
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        <h1>To Do List</h1>
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            ongoingTodo={ongoingTodo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders info text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Enter new task/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders buttons', () => {
+  render(<App />);
+  const linkElement = screen.getByRole("button");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders input', () => {
+  render(<App />);
+  const linkElement = screen.getByRole("input");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders header', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/To Do List/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+describe("complete button", () => {
+  test("complete button is in the document", () => {
+    render(<App />)
+    const button = screen.getByText("Complete")
+    expect(button).toBeInTheDocument()
+  })
+})
